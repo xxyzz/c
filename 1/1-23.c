9cee5074b399019c6a63c377c38fcc6f31dec770
xxyzz <gitpull@protonmail.com>
2020-07-27 11:41:01 +0800

    1-23 check inside quotes

--- 1/1-23.c
+++ 1/1-23.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  int c, inside_single = 0, inside_multi = 0, previous = ' ';
-  while((c = getchar()) != EOF) {
+  int c, inside_single = 0, inside_multi = 0,
+    inside_quote = 0, previous = ' ';
+  while ((c = getchar()) != EOF) {
     if (inside_single) {
       if (c == '\n')
         inside_single = 0;
     } else if (inside_multi) {
       if (previous == '*' && c == '/')
         inside_multi = 0;
+    } else if (inside_quote) {
+      if (previous != '\\' && c == '"')
+        inside_quote = 0;
+      putchar(c);
     } else {
       if (c == '/') {
         int next = getchar();
@@ -18,8 +23,11 @@ int main() {
           inside_multi = 1;
         else if (next == EOF)
           break;
-      } else
+      } else {
+        if (c == '"')
+          inside_quote = 1;
         putchar(c);
+      }
     }
     previous = c;
   }
