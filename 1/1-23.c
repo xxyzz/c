57a3f2d495adef568aa22b03a847b64a35a5efee
xxyzz <gitpull@protonmail.com>
2020-07-28 11:50:56 +0800

    1-23 single quote

--- 1/1-23.c
+++ 1/1-23.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  int c, inside_single = 0, inside_multi = 0,
-    inside_quote = 0, previous = ' ';
+  int c, inside_single = 0, inside_multi = 0, single_quote = 0, double_quote= 0, previous = ' ';
   while ((c = getchar()) != EOF) {
-    if (inside_single) {
+    if (inside_single) { // inside single line comment
       if (c == '\n')
         inside_single = 0;
-    } else if (inside_multi) {
+    } else if (inside_multi) { // inside multi-line comment
       if (previous == '*' && c == '/')
         inside_multi = 0;
-    } else if (inside_quote) {
+    } else if (single_quote) {
+      if (previous != '\\' && c == '\'')
+        single_quote = 0;
+      putchar(c);
+    } else if (double_quote) {
       if (previous != '\\' && c == '"')
-        inside_quote = 0;
+        double_quote = 0;
       putchar(c);
     } else {
       if (c == '/') {
@@ -23,11 +26,14 @@ int main() {
           inside_multi = 1;
         else if (next == EOF)
           break;
-      } else {
-        if (c == '"')
-          inside_quote = 1;
+      } else if (c == '"') {
+        double_quote = 1;
         putchar(c);
-      }
+      } else if (c == '\'') {
+        single_quote = 1;
+        putchar(c);
+      } else
+        putchar(c);
     }
     previous = c;
   }
