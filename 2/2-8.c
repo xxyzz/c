1ea106a59023a1c96551216e7c786b2dd5003ad1
xxyzz <gitpull@protonmail.com>
2020-07-31 09:37:39 +0800

    2-8 rotate bits

---
+++ 2/2-8.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int wordlength(void);
+unsigned rightrot(unsigned x, int n);
+
+int main() {
+  printf("word length: %d\n", wordlength()); // 32
+  // 0000 0000 0000 0000 0000 0000 0000 0011 = 3
+  // 0011 0000 0000 0000 0000 0000 0000 0000 = 805306368
+  printf("rightrot(3, 4): %d\n", rightrot(3, 4));
+  return 0;
+}
+
+int wordlength() {
+  int len;
+  unsigned i = (unsigned)~0;
+  for (len = 1; (i = i >> 1) > 0; len++)
+    ;
+  return len;
+}
+
+unsigned rightrot(unsigned x, int n) {
+  int word_len = wordlength();
+  if ((n = n % word_len) > 0) {
+    unsigned right_bits = x & (unsigned)~(~0 << n);
+    right_bits = right_bits << (word_len - n);
+    x = (x >> n) | right_bits;
+  }
+  return x;
+}
--- 2/README.md
+++ 2/README.md
@@ -45,3 +45,7 @@ Write a function `setbits(x,p,n,y)` that returns `x` with the `n` bits that begi
 #### Exercise 2-7
 
 Write a function `invert(x,p,n)` that returns `x` with the `n` bits that begin at position `p` inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
+
+#### Exercise 2-8
+
+Write a function `rightrot(x,n)` that returns the value of the integer `x` rotated to the right by `n` bit positions.
