da7eead9d7cd8ab80a23a52d89f7b403649b122e
xxyzz <gitpull@protonmail.com>
2020-08-02 22:50:21 +0800

    include makefile

---
+++ common.mk
@@ -0,0 +1,11 @@
+CC = clang
+CFLAGS = -Weverything
+TARGETS = $(foreach i,$(shell seq $(EXERCISES)),$(CHAPTER)-$i.out)
+
+all: $(TARGETS)
+
+%.out: %.c
+	$(CC) $^ -o $@ $(CFLAGS)
+
+clean:
+	rm -f ./*.out
