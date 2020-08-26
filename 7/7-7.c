2c1ae2525f80ef746dd39e6fd5c8af06dfb94faa
xxyzz <gitpull@protonmail.com>
2020-08-26 19:20:34 +0800

    7-7 free pointers and print filename

--- 7/7-7.c
+++ 7/7-7.c
@@ -6,13 +6,14 @@
 
 enum { EXCEPT = 1, NUMBER = 2, FROMFILE = 4 };
 
-void search_pattern(FILE *, char *, int);
+void search_pattern(FILE *, char *, char *, int);
 
 int main(int argc, char *argv[]) {
   int opt, flags = 0;
   size_t filenum = 0;
   char *token, *pattern = NULL;
   FILE *files[RLIMIT_NOFILE];
+  char *fnames[RLIMIT_NOFILE];
   while ((opt = getopt(argc, argv, "xnf:p:")) != -1) {
     switch (opt) {
     case 'x':
@@ -23,11 +24,16 @@ int main(int argc, char *argv[]) {
       break;
     case 'f':
       flags |= FROMFILE;
-      while ((token = strsep(&optarg, ",")) != NULL)
-        if ((files[filenum++] = fopen(token, "r")) == NULL) {
+      while ((token = strsep(&optarg, ",")) != NULL && filenum < RLIMIT_NOFILE) {
+        if ((files[filenum] = fopen(token, "r")) == NULL) {
           fprintf(stderr, "Can't open %s\n", token);
           exit(EXIT_FAILURE);
         }
+        if ((fnames[filenum++] = strdup(token)) == NULL) {
+          fprintf(stderr, "strdup failed, hambeurgers.\n");
+          exit(EXIT_FAILURE);
+        }
+      }
       break;
     case 'p':
       if ((pattern = strdup(optarg)) == NULL) {
@@ -50,14 +56,17 @@ int main(int argc, char *argv[]) {
 
   if (flags & FROMFILE)
     for (size_t i = 0; i < filenum; i++)
-      search_pattern(files[i], pattern, flags);
+      search_pattern(files[i], pattern, fnames[i], flags);
   else
-    search_pattern(stdin, pattern, flags);
+    search_pattern(stdin, pattern, NULL, flags);
 
+  free(pattern);
+  for (size_t i = 0; i < filenum; i++)
+    free(fnames[i]);
   return 0;
 }
 
-void search_pattern(FILE *f, char *pattern, int flags) {
+void search_pattern(FILE *f, char *pattern, char *filename, int flags) {
   char *line = NULL;
   size_t len = 0;
   ssize_t nread;
@@ -66,9 +75,15 @@ void search_pattern(FILE *f, char *pattern, int flags) {
   while ((nread = getline(&line, &len, f)) != -1) {
     lineno++;
     if ((strstr(line, pattern) != NULL) != (flags & EXCEPT)) {
+      if (filename) {
+        printf("%s\n", filename);
+        filename = NULL;
+      }
       if (flags & NUMBER)
         printf("%ld: ", lineno);
       printf("%s", line);
     }
   }
+
+  free(line);
 }
